1a6b4d3d1f1922e89f1cefa787d371f1b4302d68
Leo Ahopalo <bgs208@myy-haaga.helia.fi>
2022-08-31 13:47:21 +0300

    tehtävä tehty

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([]);
+
+  const add = () => {
+    setData([...data, { key: value, id: 1 }]);
+    setValue("");
+  };
+  const clearAllItems = () => {
+    setData([]);
+  };
+
+  console.log(...data);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.Text}>
+        <TextInput value={value} onChangeText={(text) => setValue(text)} />
+      </View>
+      <View style={styles.ButtonView}>
+        <TouchableOpacity style={styles.Button} onPress={add}>
+          <Text>ADD</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.Button} onPress={clearAllItems}>
+          <Text>CLEAR</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.listStyle}>
+        <Text>Shopping List</Text>
+      </View>
+      <View style={styles.list}>
+        <FlatList
+          data={data}
+          renderItem={({ item, index }) => <Text key={index}>{item.key}</Text>}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +52,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Text: {
+    borderWidth: 2,
+    borderColor: "gray",
+    width: "50%",
+    margin: 5,
+  },
+  ButtonView: {
+    marginTop: 4,
+    flexDirection: "row",
+  },
+
+  Button: {
+    height: 40,
+    width: 60,
+    alignItems: "center",
+    marginHorizontal: 1,
+    borderRadius: 2,
+    justifyContent: "center",
+    backgroundColor: "gray",
+    borderWidth: 2,
+  },
+  listStyle: {
+    marginTop: 40,
+    width: "40%",
+    alignItems: "center",
+    borderBottomWidth: 2,
+  },
+  list: {
+    width: "40%",
+    alignItems: "center",
   },
 });
